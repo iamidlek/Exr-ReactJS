436db577613c4fa197bdaf807e6286178c787519
iamidlek <iamidlek@naver.com>
2021-12-14 12:53:51 +0900

    about form and event

---
+++ src/studys/TypescriptForm.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export const TypescriptForm = () => {
+  const [value, setValue] = useState("");
+  // onchange 함수가 HTML의 input요소에서 발생할 것을 알려줌
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // reactJS 에서는 target을 currentTarget으로 이용하게 함
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  // form의 경우 또한 어떤 요소에서 발생하는지 정의해 준다
+  // reactjs 의 이벤트 최적화 => SyntheticEvent(모든 브라우저에서 이벤트를 동일하게 처리하기 위한 이벤트 래퍼)
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+};
