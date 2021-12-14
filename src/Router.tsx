7dba7fe8a64332dc1ec98864864123f43db91f1e
iamidlek <iamidlek@naver.com>
2021-12-14 23:38:32 +0900

    nested routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,8 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinId" element={<Coin />} />
+        {/* /:coinId 뒤에 /* 는 중첩을 위함 */}
+        <Route path="/:coinId/*" element={<Coin />} />
         <Route path="/" element={<Coins />} />
       </Routes>
     </BrowserRouter>
