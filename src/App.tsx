42b730ff054c0dc16b27c67b988394fab10e068f
iamidlek <iamidlek@naver.com>
2021-12-14 12:24:27 +0900

    setting for study

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import StyledComponents from "./studys/StyledComponents.js";
+import TypeingTheProps from "./studys/TypeingTheProps";
+
+function App() {
+  return (
+    <div>
+      {/* <StyledComponents /> */}
+      <TypeingTheProps bgColor="red" />
+    </div>
+  );
+}
+
+export default App;
